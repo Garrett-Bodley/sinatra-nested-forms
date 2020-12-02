150e36201e3f55e7fa07eb44d1642e5ee5545af6
Garrett <Garrett.Bodley@gmail.com>
2020-12-01 22:37:17 -0500

    commit tree fa0ae98c0bb4d65df1fc716dec34f6798ce57088

--- app.rb
+++ app.rb
@@ -4,6 +4,27 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      # pirate_hash = Hash[params[:pirate].map{|key, value| [key.gsub(/:/, '').to_sym, value]}].tap{|h| h.delete(:ships)}
+      # ship1_hash = Hash[params[:pirate]["ships"][0].map{|key, value| [key.gsub(/:/, '').to_sym, value]}]
+      # ship2_hash = Hash[params[:pirate]["ships"][1].map{|key, value| [key.gsub(/:/, '').to_sym, value]}]
+
+      @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+      erb :"pirates/show"
+    end
 
   end
 end
